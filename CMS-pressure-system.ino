8429f942931324f061c9e9457617bf135438d507
will <william.m.dove@gmail.com>
2025-10-03 18:05:50 -0400

    WIP Python serial communication

--- CMS-pressure-system.ino
+++ CMS-pressure-system.ino
@@ -1,9 +1,27 @@
+
+int serial_test_var;
+
 void setup() {
   // put your setup code here, to run once:
+
+  // For serial communication:
+
+    Serial.begin(115200);   // Start serial communication
+    Serial.setTimeout(100);   // Set timeout when waiting for serial input to 100 ms
+
+  // For the Python manual run:
+
+
+
+
+  // For the arduino automated run:
+
+  // Map acions to Arduino pin numbers
   int pressurize= 7;
   int release = 4;
   int vacuum = 12;
   int button = 2;
+  // Set pins as input or output
   pinMode(pressurize, OUTPUT);
   pinMode(release, OUTPUT);
   pinMode(vacuum, OUTPUT);
@@ -12,25 +30,53 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int pressurize= 7;
+
+
+  // Variables
+
+  int pressurize= 7;  //not sure why these are in the loop as well
   int release = 4;
   int vacuum = 12;
   int button = 2;
   int t = 300000;                         // number of time at each lower stage (in ms) (5 mins)
   int x = 5;                              //number of stages before max
   int a = 0;
+
+
+  // Serial test
+
+  if (Serial.available() > 0) {                        // Wait for serial info
+    String inputString = Serial.readStringUntil('\n');  // read until newline
+    int serial_test_var = inputString.toInt();
+    //serial_test_var = Serial.readString().toInt();    
+    Serial.println(serial_test_var + 1);
+  }
+
+  // Automated arduino run code:
+
+  // Wait for an input (button pressed) to begin automated run
   while(digitalRead(button)==LOW){}       // press button to initialize test once pipe is attached
-  digitalWrite(vacuum,HIGH);              //leak check inital pipe attachment
+  //leak check inital pipe attachment
+  digitalWrite(vacuum,HIGH);          
   delay(500);
-  while(digitalRead(button)==LOW){}       //press button to close leak checker
-  digitalWrite(vacuum,LOW);               //close leak checker
+
+  // Wait until button press to close leak checker
+  while(digitalRead(button)==LOW){}       
+  //close leak checker
+  digitalWrite(vacuum,LOW);               
   delay(500);
-  while(digitalRead(button)==LOW){}       //press button to pressurize pipe
-  digitalWrite(pressurize,HIGH);          //pressurize pipe for the first time @ 472 PSI
+
+  //press button to pressurize pipe
+  while(digitalRead(button)==LOW){}    
+  //pressurize pipe for the first time @ 472 PSI
+  digitalWrite(pressurize,HIGH);          
   delay(5000);
   digitalWrite(pressurize,LOW);
   delay(600000);                          //hold at pressure for 10 minutes
-  while(digitalRead(button)==LOW){}       //press button to start the looping for building up pressure
+
+  //press button to start the looping for building up pressure
+  while(digitalRead(button)==LOW){}       
+  // Once button is pressed:
   while(a!=x){                            //loop through all the pressure stages before max pressure (pressure adjusted manually) (1175-1410-1645-1880-2115)
     digitalWrite(pressurize,HIGH);        //pressurize pipe
     delay(5000);
@@ -39,6 +85,7 @@ void loop() {
     while(digitalRead(button)==LOW){}     //wait for button input to begin next stage (manually adjust pressure regulator before pressing)
     a=a+1;
   }
+
   digitalWrite(pressurize,HIGH);          //pressurize pipe at max pressure (2350)
   delay(5000);
   digitalWrite(pressurize,LOW);
@@ -50,9 +97,16 @@ void loop() {
   digitalWrite(release,HIGH);             //release
   delay(5000);
   digitalWrite(release,LOW);
-  while(digitalRead(button)==LOW){}       //wait for button input to begin next stage (manually adjust pressure regulator before pressing)
+
+  //wait for button input to begin next stage (manually adjust pressure regulator before pressing)
+  while(digitalRead(button)==LOW){}       
   digitalWrite(vacuum,HIGH);              //These three lines are the leak checker code, move them to the loop if we need to
+
   while(digitalRead(button)==LOW){}
   delay(500);
   digitalWrite(vacuum,LOW);
 }
+
+
+
+
--- cms-python.py
+++ cms-python.py
@@ -1,3 +1,40 @@
-'''
-This is our python file for the thingy
-'''
+
+import serial
+from serial.tools import list_ports
+import time
+
+def find_arduino():
+    '''
+    Finds the serial port that the arduino is plugged into.
+    '''
+    ports = list_ports.comports()
+    for p in ports:
+        if "Arduino" in p.description or "USB-SERIAL" in p.description:
+            return p.device
+    return None
+
+# Find the serial port with the arduino
+port = find_arduino()
+if port:
+    print("Found Arduino on", port)
+else:
+    print("Arduino not found.")
+
+# Define the arduino
+arduino = serial.Serial(port=port, baudrate=115200, timeout=.1) # Same baudrate, timeout as in .ino
+
+
+# TEST FUNCTION
+
+def write_read(serial_test_var): 
+    arduino.write(bytes(f'{serial_test_var}\n', 'utf-8')) # Added newline so arduino reads it
+    time.sleep(0.05) 
+    data = arduino.readline() 
+    data_string = data.decode('utf-8').strip()  # Decode bytes to string and strip newline/carriage return
+    return data_string 
+
+while True: 
+	in_value = input("Enter a number: ") # Taking input from user 
+	out_value = write_read(in_value) 
+	print(out_value) # printing the value 
+
