c2b73e8b934d7b6e488155bab065e7bf446b1832
will <william.m.dove@gmail.com>
2025-10-07 18:22:09 -0400

    Basic open/close commands working

--- CMS-pressure-system.ino
+++ CMS-pressure-system.ino
@@ -2,10 +2,10 @@
 // Variables
 
 // Map actions to Arduino pin numbers
-int pressurize= 7;
-int release = 4;
-int vacuum = 12;
-int button = 2;
+const int PRESSURIZE = 7;
+const int RELEASE = 4;
+const int VACUUM = 12;
+const int BUTTON = 2;
 
 //Set up variables 
 int serial_test_var;
@@ -23,10 +23,10 @@ void setup() {
   Serial.setTimeout(100);   // Set timeout when waiting for serial input to 100 ms
 
   // Set pins as input or output
-  pinMode(pressurize, OUTPUT);
-  pinMode(release, OUTPUT);
-  pinMode(vacuum, OUTPUT);
-  pinMode(button, INPUT_PULLUP);
+  pinMode(PRESSURIZE, OUTPUT);
+  pinMode(RELEASE, OUTPUT);
+  pinMode(VACUUM, OUTPUT);
+  pinMode(BUTTON, INPUT_PULLUP);
 
 }
 
@@ -34,75 +34,100 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  // Serial test
-
   // Wait for serial info
   while (!Serial.available()); 
 
   // Read input
   String inputString = Serial.readStringUntil('\n');  // read until newline
+  inputString.trim();
 
   if (inputString == "test") {
     while (!Serial.available()); 
     serial_test_var = Serial.readStringUntil('\n').toInt();
-    //serial_test_var = Serial.readString().toInt(); 
     Serial.println(serial_test_var + 1);
+  }
+
+  else if (inputString == "open vacuum") {
+    digitalWrite(VACUUM, HIGH);   
+    Serial.println("Leak checker opened.");
+  }
+  else if (inputString == "close vacuum") {
+    digitalWrite(VACUUM, LOW);
+    Serial.println("Leak checker closed.");
+  }
+  else if (inputString == "open pressure") {
+    digitalWrite(PRESSURIZE, HIGH);
+    Serial.println("Pressure valve opened.");
+  }
+  else if (inputString == "close pressure") {
+    digitalWrite(PRESSURIZE, LOW);
+    Serial.println("Pressure valve closed.");
+  }
+  else if (inputString == "open release") {
+    digitalWrite(RELEASE, HIGH);
+    Serial.println("Release valve opened.");
+  }
+  else if (inputString == "close release") {
+    digitalWrite(RELEASE, LOW);
+    Serial.println("Release valve closed.");
   }
 
   else if (inputString == "automated") {
     // Automated arduino run code:
+    Serial.println("Starting automated sequence.");
+    a = 0; // reset stage counter
 
     // Wait for an input (button pressed) to begin automated run
-    while(digitalRead(button)==LOW){}       // press button to initialize test once pipe is attached
+    while(digitalRead(BUTTON)==LOW){}       // press button to initialize test once pipe is attached
     //leak check inital pipe attachment
-    digitalWrite(vacuum,HIGH);          
+    digitalWrite(VACUUM,HIGH);          
     delay(500);
 
     // Wait until button press to close leak checker
-    while(digitalRead(button)==LOW){}       
+    while(digitalRead(BUTTON)==LOW){}       
     //close leak checker
-    digitalWrite(vacuum,LOW);               
+    digitalWrite(VACUUM,LOW);               
     delay(500);
 
     //press button to pressurize pipe
-    while(digitalRead(button)==LOW){}    
+    while(digitalRead(BUTTON)==LOW){}    
     //pressurize pipe for the first time @ 472 PSI
-    digitalWrite(pressurize,HIGH);          
+    digitalWrite(PRESSURIZE,HIGH);          
     delay(5000);
-    digitalWrite(pressurize,LOW);
+    digitalWrite(PRESSURIZE,LOW);
     delay(600000);                          //hold at pressure for 10 minutes
 
     //press button to start the looping for building up pressure
-    while(digitalRead(button)==LOW){}       
+    while(digitalRead(BUTTON)==LOW){}       
     // Once button is pressed:
     while(a!=x){                            //loop through all the pressure stages before max pressure (pressure adjusted manually) (1175-1410-1645-1880-2115)
-      digitalWrite(pressurize,HIGH);        //pressurize pipe
+      digitalWrite(PRESSURIZE,HIGH);        //pressurize pipe
       delay(5000);
-      digitalWrite(pressurize,LOW);
+      digitalWrite(PRESSURIZE,LOW);
       delay(t);                             //hold at pressure
-      while(digitalRead(button)==LOW){}     //wait for button input to begin next stage (manually adjust pressure regulator before pressing)
+      while(digitalRead(BUTTON)==LOW){}     //wait for button input to begin next stage (manually adjust pressure regulator before pressing)
       a=a+1;
     }
 
-    digitalWrite(pressurize,HIGH);          //pressurize pipe at max pressure (2350)
+    digitalWrite(PRESSURIZE,HIGH);          //pressurize pipe at max pressure (2350)
     delay(5000);
-    digitalWrite(pressurize,LOW);
+    digitalWrite(PRESSURIZE,LOW);
     delay(1800000);                         //30 minutes
-    digitalWrite(pressurize,HIGH);          //pressurize pipe (2115)
+    digitalWrite(PRESSURIZE,HIGH);          //pressurize pipe (2115)
     delay(5000);
-    digitalWrite(pressurize,LOW);
+    digitalWrite(PRESSURIZE,LOW);
     delay(t);                               //hold at pressure
-    digitalWrite(release,HIGH);             //release
+    digitalWrite(RELEASE,HIGH);             //release
     delay(5000);
-    digitalWrite(release,LOW);
+    digitalWrite(RELEASE,LOW);
 
     //wait for button input to begin next stage (manually adjust pressure regulator before pressing)
-    while(digitalRead(button)==LOW){}       
-    digitalWrite(vacuum,HIGH);              //These three lines are the leak checker code, move them to the loop if we need to
+    while(digitalRead(BUTTON)==LOW){}       
+    digitalWrite(VACUUM,HIGH);              //These three lines are the leak checker code, move them to the loop if we need to
 
-    while(digitalRead(button)==LOW){}
+    while(digitalRead(BUTTON)==LOW){}
     delay(500);
-    digitalWrite(vacuum,LOW);
+    digitalWrite(VACUUM,LOW);
   } // End automated run
 
   else {
